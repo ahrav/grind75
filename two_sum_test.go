2769c988b5123e6b8ef31f5480f8b1a7ad6bf71a
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2023-10-07 15:42:12 -0700

    setup + two_sum

---
+++ two_sum_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwoSum(t *testing.T) {
+	testCases := []struct {
+		nums   []int
+		target int
+		want   []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+		{[]int{3, 3}, 6, []int{0, 1}},
+	}
+
+	for _, tc := range testCases {
+		got := twoSum(tc.nums, tc.target)
+		assert.Equal(t, tc.want, got)
+	}
+}
