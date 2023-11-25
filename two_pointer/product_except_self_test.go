f044a1959dd5c3922b32e5cc549e61698745a1dd
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2023-11-25 10:44:28 -0800

    add test case

--- two_pointer/product_except_self_test.go
+++ two_pointer/product_except_self_test.go
@@ -1,6 +1,26 @@
 package two_pointer
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProductExceptSelf(t *testing.T) {
+	testCases := []struct {
+		nums []int
+		want []int
+	}{
+		{[]int{1, 2, 3, 4}, []int{24, 12, 8, 6}},
+		{[]int{-1, 1, 0, -3, 3}, []int{0, 0, 9, 0, 0}},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, []int{362880, 181440, 120960, 90720, 72576, 60480, 51840, 45360, 40320}},
+	}
+
+	for _, tc := range testCases {
+		got := productExceptSelf(tc.nums)
+		assert.Equal(t, tc.want, got)
+	}
+}
 
 func genLargeSlice() []int {
 	var nums []int
